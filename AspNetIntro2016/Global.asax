093a8d4aa844443b076030eba8ac3a1f8c0f2ff1
Judas1337 <daniel.persson@xlent.se>
2016-01-20 15:49:19 +0100

    Added default setup for VS Web MVC-project

---
+++ AspNetIntro2016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetIntro2016.MvcApplication" Language="C#" %>
---
+++ AspNetIntro2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetIntro2016.Startup))]
+namespace AspNetIntro2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
